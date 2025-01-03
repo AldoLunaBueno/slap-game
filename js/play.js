f448bd2d42bca908818001ae6ce49a5b6590a155
AldoLunaBueno <alunab@uni.pe>
2025-01-03 13:39:54 -0500

    Human hand and basic gameplay dynamic

--- js/play.js
+++ js/play.js
@@ -1,45 +1,80 @@
 export function init(deckId) {
-  const options = $$("section.play .options article");
-  const hand = $("section.play .hand.human");
+  const options = $$(".options article");
+  const hand = $(".hand.human");
+  const handplace = hand.parentElement;
   hand.draggable = true;
-  let sample = null;
-
-  hand.addEventListener("mousedown", () => {
-    const rand = _.random(0, 3);
-    let utterance = new SpeechSynthesisUtterance(
-      `Where is the ${sample[rand].name}?`
-    );
-    utterance.lang = "en-US";
-    speechSynthesis.speak(utterance);
-  });
-
-  options.forEach((option) => {
-    option.addEventListener("dragover", (ev) => {
-      ev.preventDefault();
-    });
-
-    option.addEventListener("drop", (ev) => {
-      ev.preventDefault();
-      const draggable = $("#draggable");
-      const w = draggable.offsetWidth;
-      const h = draggable.offsetHeight;
-      draggable.parentElement.removeChild(draggable);
-      draggable.style.width = w + "px";
-      draggable.style.height = h + "px";
-      option.appendChild(draggable);
-    });
-  });
 
   fetch(`/assets/decks/${deckId}/data.json`)
     .then((response) => response.json())
     .then((data) => {
-      sample = _.sampleSize(data.images, 4);
-      for (let i = 0; i < 4; i++) {
-        const image = sample[i];
-        const imageElement = document.createElement("img");
-        imageElement.src = `assets/decks/${deckId}/images/${image.src}`;
-        imageElement.draggable = false;
-        options[i].appendChild(imageElement);
-      }
+      /**
+       * Nuevas imágenes se cargan
+       * La mano vuelve a su lugar y espera ser presionada
+       * La voz comienza cuando la mano es presionada
+       * Pasan 2 segundos (no importar si la mano se vuelve a presionar o sigue siendo arrastrada)
+       * Repetimos todo
+       */
+      let sample = setImageSample(data, deckId, options);
+
+      hand.addEventListener("mousedown", function loop() {
+        options.forEach((option) => {
+          option.addEventListener("dragover", (ev) => {
+            ev.preventDefault();
+          });
+
+          option.addEventListener("drop", handleDrop);
+        });
+        speak(sample);
+        this.removeEventListener("mousedown", loop);
+        setTimeout(() => {
+          handplace.appendChild(hand);
+          options.forEach((option) => {
+            option.innerHTML = "";
+          });
+          sample = setImageSample(data, deckId, options);
+          handplace.appendChild(hand);
+          this.addEventListener("mousedown", loop);
+          options.forEach((option) => {
+            option.removeEventListener("drop", handleDrop);
+          });
+        }, 2500);
+      });
     });
+}
+
+function handleDrop(ev) {
+  ev.preventDefault();
+  const draggable = $("#draggable");
+  const w = draggable.offsetWidth;
+  const h = draggable.offsetHeight;
+  draggable.parentElement.removeChild(draggable);
+  draggable.style.width = w + "px";
+  draggable.style.height = h + "px";
+  if (ev.target.nodeName == "IMG") {
+    ev.target.parentElement.appendChild(draggable);
+  } else {
+    ev.target.appendChild(draggable);
+  }
+  
+}
+
+function speak(sample) {
+  const rand = _.random(0, 3);
+  let utterance = new SpeechSynthesisUtterance(
+    `Where is the ${sample[rand].name}?`
+  );
+  utterance.lang = "en-US";
+  speechSynthesis.speak(utterance);
+}
+
+function setImageSample(data, deckId, options) {
+  const sample = _.sampleSize(data.images, 4);
+  for (let i = 0; i < 4; i++) {
+    const image = sample[i];
+    const imageElement = document.createElement("img");
+    imageElement.src = `assets/decks/${deckId}/images/${image.src}`;
+    imageElement.draggable = false;
+    options[i].appendChild(imageElement);
+  }
+  return sample;
 }
